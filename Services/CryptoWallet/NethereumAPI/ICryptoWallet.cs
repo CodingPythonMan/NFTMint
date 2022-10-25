45fb93475518d9989348cc2a9377f69d0a25c59a
robo <robolab1902@naver.com>
2022-10-25 22:59:56 +0900

    Abstract 클래스 추가, Metamask js 추가

---
+++ Services/CryptoWallet/NethereumAPI/ICryptoWallet.cs
@@ -0,0 +1,106 @@
+﻿using Nethereum.JsonRpc.Client.RpcMessages;
+
+namespace NFTMint.Services.CryptoWallet.NethereumAPI
+{
+    public abstract class ICryptoWallet : IEthereumHostProvider, IMetamaskInterop
+    {
+        public string Name => throw new NotImplementedException();
+
+        public bool Available => throw new NotImplementedException();
+
+        public string SelectedAccount => throw new NotImplementedException();
+
+        public int SelectedNetwork => throw new NotImplementedException();
+
+        public bool Enabled => throw new NotImplementedException();
+
+        public event Func<string, Task> SelectedAccountChanged;
+        public event Func<bool, Task> AvailabilityChanged;
+        public event Func<int, Task> NetworkChanged;
+
+        public ValueTask<bool> CheckMetamaskAvailability()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CheckProviderAvailabilityAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<string> EnableEthereumAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> EnableProviderAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<int> GetChainID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetProviderSelectedAccountAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetProviderSelectedNetworkAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<string> GetSelectedAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Web3> GetWeb3Async()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<RpcResponseMessage> SendAsync(RpcRequestMessage rpcRequestMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<RpcResponseMessage> SendTransactionAsync(MetamaskRpcRequestMessage rpcRequestMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<string> SignAsync(string utf8Hex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> SignMessageAsync(string utf8Hex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<bool> SwitchEthereumChain(string chainId, string chainName, string symbol, int decimals, string[] rpcUrls, string[] blockExplorerUrls)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<int> IEthereumHostProvider.GetChainID()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<string> IEthereumHostProvider.SignAsync(string utf8Hex)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IEthereumHostProvider.SwitchEthereumChain(string chainId, string chainName, string symbol, int decimals, string[] rpcUrls, string[] blockExplorerUrls)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ wwwroot/js/NethereumMetamask.js
@@ -0,0 +1,195 @@
+﻿export const NethereumMetamaskInterop = {
+	Init: (provider) => {
+		NethereumMetamaskInterop.Provider = provider;
+		NethereumMetamaskInterop.SetListener();
+	},
+
+	SetListener: () => {
+		if (window.ethereum) {
+			console.log("SetListener");
+			ethereum.on("accountsChanged",
+				(accounts) => {
+					if (accounts.length === 0) {
+						console.log("all disconnect");
+						//DotNet.invokeMethodAsync('Market', 'SelectedAccountChanged', "");
+						NethereumMetamaskInterop.Provider.invokeMethodAsync('ChangeSelectedAccountAsync', "");
+					}
+					else {
+						console.log(accounts[0]);
+						//DotNet.invokeMethodAsync('Market', 'SelectedAccountChanged', accounts[0]);
+						NethereumMetamaskInterop.Provider.invokeMethodAsync('ChangeSelectedAccountAsync', accounts[0]);
+					}
+				}
+			);
+
+			ethereum.on('chainChanged',
+				(chainId) => {
+					//DotNet.invokeMethodAsync('Market', 'ChainChanged', parseInt(chainId));
+					NethereumMetamaskInterop.Provider.invokeMethodAsync('ChainChangedAsync', parseInt(chainId));
+				}
+			);
+		}
+	},
+
+	EnableEthereum: async () => {
+		try {
+			const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+			ethereum.autoRefreshOnNetworkChange = false;
+			//ethereum.on("accountsChanged",
+			//	function (accounts) {
+			//		if (accounts.length === 0) {
+			//			console.log("all disconnect");
+			//			DotNet.invokeMethodAsync('Market', 'SelectedAccountChanged', "");
+			//		}
+			//		else {
+			//			console.log(accounts[0]);
+			//			DotNet.invokeMethodAsync('Market', 'SelectedAccountChanged', accounts[0]);
+			//		}
+			//	});
+			//ethereum.on("networkChanged",
+			//	function (networkId) {
+
+			//	});
+			//ethereum.on("disconnect",
+			//	function (error) {
+			//		console.log(error);
+			//	});
+
+			return accounts[0];
+		} catch (error) {
+			console.log(error)
+			if (error.code === 4001) {
+				DotNet.invokeMethodAsync('Market', 'MetamaskUserDenied');
+			}
+			return null;
+		}
+	},
+
+	IsConnected: () => {
+		return ethereum.isConnected();
+	},
+
+	IsMetamaskAvailable: () => {
+		if (window.ethereum) return true;
+		return false;
+	},
+	GetSelectedAddress: () => {
+		console.log(ethereum);
+		return ethereum.selectedAddress;
+	},
+
+	Request: async (message) => {
+		try {
+
+			let parsedMessage = JSON.parse(message);
+			console.log(parsedMessage);
+			const response = await ethereum.request(parsedMessage);
+			let rpcResonse = {
+				jsonrpc: "2.0",
+				result: response,
+				id: parsedMessage.id,
+				error: null
+			}
+			console.log(rpcResonse);
+
+			return JSON.stringify(rpcResonse);
+		} catch (e) {
+			console.log(e);
+			console.log(message);
+			let parsedMessage = JSON.parse(message);
+			//DotNet.invokeMethodAsync('Market', 'MetamaskUserDenied');
+			let rpcResonseError = {
+				jsonrpc: "2.0",
+				id: parsedMessage.id,
+				Error: {
+					Message: e.message,
+				}
+			}
+			return JSON.stringify(rpcResonseError);
+		}
+	},
+
+	Send: async (message) => {
+		return new Promise(function (resolve, reject) {
+			console.log(JSON.parse(message));
+			ethereum.send(JSON.parse(message), function (error, result) {
+				console.log(result);
+				console.log(error);
+				resolve(JSON.stringify(result));
+			});
+		});
+	},
+
+	Sign: async (utf8HexMsg) => {
+		const from = ethereum.selectedAddress;
+		const params = [utf8HexMsg, from];
+		const method = 'personal_sign';
+
+		return await ethereum.request({
+			method, params
+		}).then((result) => {
+			return result;
+		}).catch((error) => {
+			return '';
+		});
+	},
+
+	GetChainID: async () => {
+		const chainId = await ethereum.request({ method: 'eth_chainId' });
+		return parseInt(chainId);
+	},
+
+	SwitchEthereumChain: async (
+		chainId,
+		chainName,
+		symbol,
+		decimals,
+		rpcUrls,
+		blockExplorerUrls
+	) => {
+		return await ethereum
+			.request({
+				method: 'wallet_switchEthereumChain',
+				params: [{ chainId: chainId }],
+			})
+			.then((result) => {
+				return true;
+			})
+			.catch((error) => {
+				if (error.code === 4001) {
+					// EIP-1193 userRejectedRequest error
+					console.log("wallet_switchEthereumChain userRejectedRequest");
+				}
+				else if (error.code === 4902) {
+					//Unrecognized chain ID
+					 return ethereum
+						.request({
+							method: 'wallet_addEthereumChain',
+							params: [
+								{
+									chainId: chainId,
+									chainName: chainName,
+									nativeCurrency: {
+										symbol: symbol,
+										decimals: decimals,
+									},
+									rpcUrls: rpcUrls,
+									blockExplorerUrls: blockExplorerUrls
+								},
+							],
+						})
+						.then((result) => {
+							return true;
+						})
+						.catch((error) => {
+							console.log(error);
+							return false;
+						});
+				}
+				else {
+					console.log(error);
+				}
+				return false;
+			});
+	},
+}
