125b792137dc856181b7593219bfe6f838ef46bb
김주영 <robolab1902@ringgames.com>
2022-11-04 13:54:24 +0900

    Database 추가

---
+++ Databases/MyDB_MDB/dbo/Tables/tMarketplaceRune.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tMarketplaceRune]
+(
+	[cOwnerAddress]		[dbo].[ADDRESS_LENGTH] ,	
+	[cIndex]			INT NOT NULL,  
+	[cStack]			INT CONSTRAINT [DF_tMarketplaceRune_cStack] DEFAULT 0 NOT NULL, 
+
+	CONSTRAINT [PK_tMarketplaceRune] PRIMARY KEY ([cOwnerAddress],[cIndex] )
+)
---
+++ Databases/MyDB_MDB/dbo/Tables/tWalletAccount.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[tWalletAccount]
+(
+    [cWalletAddress]	[dbo].[ADDRESS_LENGTH] NOT NULL,
+    [cIdentityUnique]	BIGINT CONSTRAINT [DF_tWalletAccount_cIdentityUnique] DEFAULT 0 NOT NULL,
+    [cAccountUnique] BIGINT CONSTRAINT [DF_tWalletAccount_cAccountUnique] DEFAULT 0 NOT NULL,  
+    [cResetTimeStamp] BIGINT CONSTRAINT [DF_tWalletAccount_cResetTimeStamp] DEFAULT 0 NOT NULL, 
+    [cFreeCount] INT CONSTRAINT [DF_tWalletAccount_cFreeCount] DEFAULT 0 NOT NULL, 
+    CONSTRAINT [PK_tWalletAccount] PRIMARY KEY CLUSTERED ([cWalletAddress])
+)
+
+GO
+CREATE NONCLUSTERED INDEX [IX_tWalletAccount_cIdentityUnique]
+    ON [dbo].[tWalletAccount]([cIdentityUnique]);
+
+GO
+CREATE NONCLUSTERED INDEX [IX_tWalletAccount_cAccountUnique]
+    ON [dbo].[tWalletAccount]([cAccountUnique]);
